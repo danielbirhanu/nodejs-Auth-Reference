7cd227b898c8f912649ff7995da542cdb492f02d
danielbirhanu <danielbir49@gmail.com>
2025-03-10 14:11:55 +0300

    Restrict access to some end points using Auth middleware

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ require('dotenv').config()
 const express = require('express')
 const connectToDb = require('./database/db')
 const authRoute = require('./routes/Auth-routes')
+const homeRoute = require('./routes/home-routes')
+const adminRoute = require('./routes/admin-routes')
 
 connectToDb()
 
@@ -11,6 +13,8 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.json())
 app.use('/api/auth', authRoute)
+app.use('/api/home', authRoute)
+app.use('/api/admin', authRoute)
 
 app.listen(PORT, () => (
     console.log(`Server running successfully on port ${PORT}`)
