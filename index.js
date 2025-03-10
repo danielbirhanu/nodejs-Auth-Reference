e4b022f20234ef26b32899956702d89645dcf728
danielbirhanu <danielbir49@gmail.com>
2025-03-10 11:51:15 +0300

    Implement register functionality

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 require('dotenv').config()
 const express = require('express')
 const connectToDb = require('./database/db')
+const authRoute = require('./routes/Auth-routes')
 
 connectToDb()
 
 const app = express()
 
 const PORT = process.env.PORT || 5000
+
+app.use(express.json())
+app.use('/api/auth', authRoute)
 
 app.listen(PORT, () => (
     console.log(`Server running successfully on port ${PORT}`)
